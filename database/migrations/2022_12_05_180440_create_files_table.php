6bbe26f868d3da24b6aeacbcbc5a10dd5db2526b
Sasha Buljevic <sasha_buljevic@hotmail.com>
2022-12-05 23:01:32 +0100

    add files table

---
+++ database/migrations/2022_12_05_180440_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->string('mime_type');
+            $table->string('path');
+            $table->integer('user_id')->nullable();
+            $table->unsignedBigInteger('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
